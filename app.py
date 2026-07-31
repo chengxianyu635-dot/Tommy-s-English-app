f9b1b174b0cdbaf9b6e8c3fbb31866f43b6ec139
chengxianyu635-dot <chengxianyu635@gmail.com>
2026-07-31 16:57:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -112,13 +112,13 @@ def save_database(db):
 api_key = "ark-dccbbfe2-ee90-43c2-914c-7bac589bfd2d-75676"
 try:
     if "DEEPSEEK_API_KEY" in st.secrets:
-        api_key = st.secrets["$ARK_API_KEY"]
+        api_key = st.secrets["ARK_API_KEY"]
 except:
     pass
 
 client = OpenAI(
     api_key=api_key if api_key else "ark-dccbbfe2-ee90-43c2-914c-7bac589bfd2d-75676", 
-    base_url="https://ark.cn-beijing.volces.com/api/v3/responses"
+    base_url="https://ark.cn-beijing.volces.com/api/v3"
 )
 
 # 优化后的生成函数：精简提示词 + 限制 max_tokens
